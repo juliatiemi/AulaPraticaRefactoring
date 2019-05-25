b6f312a5e9008146e7f81addf0e4361ad667fce8
Tiemi <mrshttm@gmail.com>
2019-05-24 21:12:35 -0300

    Extract Method

--- Classes/Rental.java
+++ Classes/Rental.java
@@ -32,4 +32,12 @@ class Rental {
           break;
     }
     return result;
+  }
+
+  int getFrequentRenterPoints() {
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+       return 2;
+    else
+       return 1;
+  }
 }
